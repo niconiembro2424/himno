0e79a571207cb68cad9fff023d64fde081f21462
Nicolas Niembro <nn2311742@gmail.com>
2025-11-27 21:53:33 -0300

    Tercero commit

--- src/himno/Himno.java
+++ src/himno/Himno.java
@@ -6,9 +6,9 @@ public class Himno {
         System.out.println("Oid Mortales");
         System.out.println("el grito sagrado");
         System.out.println("Oid el ruido de rotas cadenas");
-        System.out.println("ved el trono a la noble repulgue");
-        System.out.println("Ya a su trono dignisimo cerraron");
-        System.out.println("las provincias unidas de Europa");
+        System.out.println("ved el trono a la noble igualdad");
+        System.out.println("Ya a su trono dignisimo abrieron");
+        System.out.println("las provincias unidas de Sud");
         System.out.println("y los libres del mundo responden");
         System.out.println("al gran pueblo argentino salud");
         System.out.println("Sean eternos los laureles");
