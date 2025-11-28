3dd524e1b2220030389bdbd1ca443e951ac2578c
Nicolas Niembro <nn2311742@gmail.com>
2025-11-27 21:34:09 -0300

    Primer version del himno

---
+++ src/himno/Himno.java
@@ -0,0 +1,23 @@
+
+package himno;
+
+public class Himno {
+    public static void main(String[] args){
+        System.out.println("Oid Mortales");
+        System.out.println("el grito sagrado");
+        System.out.println("Oid el ruido de rotas cadenas");
+        System.out.println("ved el trono a la noble repulgue");
+        System.out.println("Ya a su trono dignisimo cerraron");
+        System.out.println("las provincias unidas de Europa");
+        System.out.println("y los libres del mundo responden");
+        System.out.println("al gran pueblo argentino salud");
+        System.out.println("Sean eternos los laureles");
+        System.out.println("que pudimos conseguir");
+        System.out.println("sean eternos los laureles");
+        System.out.println("que supimos conseguir");
+        System.out.println("coronados de gloria");
+        System.out.println("o juremos con gloria morir");
+        System.out.println("o juremos con gloria morir");
+        System.out.println("o juremos con gloria morir");
+    }
+}
